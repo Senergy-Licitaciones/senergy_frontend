efa26cce17b7f4a50ff5f075cc41689c69622a62
Marco <marco.fura@unmsm.edu.pe>
2022-05-29 08:46:47 -0500

    routefixed

--- pages/empresaAccount/licitaciones/index.tsx
+++ pages/empresaAccount/licitaciones/index.tsx
@@ -1,15 +1,15 @@
 import LayoutProveedor from "../../../components/layout/layoutProveedor";
 import TableLicitaciones from "../../../components/common/TableLicitaciones";
 import { GetServerSideProps } from "next";
-import { methodGet,methodGetAuth } from "../../../utils/fetch";
+import { methodGet, methodGetAuth } from "../../../utils/fetch";
 import { Licitacion } from "../../../types/data";
-import {verifyToken} from "../../../utils/handleJwt";
-type Props={
-    licitaciones:Licitacion[],
-    token:string
+import { verifyToken } from "../../../utils/handleJwt";
+type Props = {
+    licitaciones: Licitacion[],
+    token: string
 }
-export default function BuscadorLicitaciones({licitaciones,token}:Props){
-    return(
+export default function BuscadorLicitaciones({ licitaciones, token }: Props) {
+    return (
         <LayoutProveedor>
             <section>
                 <TableLicitaciones licitaciones={licitaciones} />
@@ -17,32 +17,32 @@ export default function BuscadorLicitaciones({licitaciones,token}:Props){
         </LayoutProveedor>
     )
 }
-export const getServerSideProps: GetServerSideProps =async(ctx)=>{
-    const data=ctx.previewData as undefined|{token?:string};
-    if(!data)return{
-        props:{},
-        redirect:{
-            destination:"/login/empresa"
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const data = ctx.previewData as undefined | { token?: string };
+    if (!data) return {
+        props: {},
+        redirect: {
+            destination: "/login/empresa"
         }
     }
-    const result=await verifyToken(data.token);
-    if(!result)return{
-        props:{},
-        redirect:{
-            destination:"/login/empresa"
+    const result = await verifyToken(data.token);
+    if (!result) return {
+        props: {},
+        redirect: {
+            destination: "/login/empresa"
         }
     }
-    if(result.type!=="proveedor")return{
-        props:{},
-        redirect:{
-            destination:"/userAccount"
+    if (result.type !== "proveedor") return {
+        props: {},
+        redirect: {
+            destination: "/userAccount"
         }
-    }   
-    const licitaciones=await methodGetAuth("licitacion/licitacionesLibre",data.token);
-    return{
-        props:{
+    }
+    const licitaciones = await methodGetAuth("licitacion/licitacionesLibres", data.token);
+    return {
+        props: {
             licitaciones,
-            token:data.token
+            token: data.token
         }
     }
 }
