0bf45f04d4aa1711734b34045af4a7da609aa1b5
Marco <marco.fura@unmsm.edu.pe>
2022-06-21 20:41:42 -0500

    first deploy

--- __tests__/components/comoon/CalendarioFechaApertura.test.tsx
+++ __tests__/components/comoon/CalendarioFechaApertura.test.tsx
@@ -4,12 +4,12 @@ import '@testing-library/jest-dom'
 import CalendarioFechaApertura from '../../../components/common/CalendarioFechaApertura'
 describe('Render calendario', () => {
   beforeEach(() => {
-    render(<CalendarioFechaApertura fechaInicioApertura='2022-07-01' fechaFinApertura='2022-07-16' />)
+    render(<CalendarioFechaApertura fechaInicioApertura='2022-07-01' fechaFinApertura='2022-07-17' />)
   })
   test('Fecha de apertura coloreada', () => {
     const liInicio = screen.getByText('1')
-    const liFin = screen.getByText('16')
-    const liOverFin = screen.getByText('17')
+    const liFin = screen.getByText('17')
+    const liOverFin = screen.getByText('18')
     expect(liInicio).toHaveClass('bg-green-200')
     expect(liFin).toHaveClass('bg-green-200')
     expect(liOverFin).not.toHaveClass('bg-green-200')
